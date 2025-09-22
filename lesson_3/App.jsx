a99922e5579555a144cef5790dbca15c6ac64684
Maksym Baikevych <112799150+YatRov@users.noreply.github.com>
2025-09-22 13:55:45 +0200

    Update App.jsx

--- lesson_3/App.jsx
+++ lesson_3/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import WalidacjaKoduPocztowego from "./components/zadanie_kod_pocztowy";
 import WeryfikacjaHasla from "./components/zadanie_2";
+import FormZamowienia from "./components/zadanie_3";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <h2>Zadanie 2</h2>
       <WeryfikacjaHasla/>
       <h2>Zadanie 3</h2>
+      <FormZamowienia/>
     </div>
   );
 }
