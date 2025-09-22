6ee333dab268ef35b6087dbaa648a397697c9fe1
Maksym Baikevych <112799150+YatRov@users.noreply.github.com>
2025-09-22 19:34:14 +0200

    Update App.jsx

--- lesson_4/App.jsx
+++ lesson_4/App.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import Owoce from "./components/zad_1";
 import Owoce_add_delete from "./components/zad_2";
 import TabKwadr from "./components/zad_3";
+import Lista from "./components/zad_4";
+import DodawanieElem from "./components/zad_5";
+import Macierz from "./components/zad_6";
+import MacierzTab from "./components/zad_7";
+import LosoweLiczby from "./components/zad_8";
+import LosowaMacierz from "./components/zad_9";
+import LosowaTab from "./components/zad_10";
+import AppOcen from "./components/zad_11";
 function App() {
   return (
     <div>
-      <Owoce />
+      <Owoce /> 
       <Owoce_add_delete />
       <TabKwadr />
+      <Lista />
+      <DodawanieElem />
+      <Macierz />
+      <MacierzTab />
+      <LosoweLiczby />
+      <LosowaMacierz />
+      <LosowaTab />
+      <AppOcen />
     </div>
   );
 }
